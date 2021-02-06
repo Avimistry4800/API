87f07cb83731082de4307df03c2a0758fb899c1b
Meghkonna Rumi <avimistry4800@gmail.com>
2021-02-07 03:03:15 +0600

    making dynamic with forEach (API)

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 fetch('https://restcountries.eu/rest/v2/all')
-.then(res => res.json())
-.then(data => displayCountries(data));
+    .then(res => res.json())
+    .then(data => displayCountries(data));
 
 const displayCountries = countries => {
     const countriesContainer = document.getElementById('countries')
-    for (let i = 0; i < countries.length; i++) {
-        const country = countries[i];
+    countries.forEach(country => {
         const countryDiv = document.createElement('div');
-        const name = document.createElement('h3');
-        name.innerText = country.name;
-        const capital = document.createElement('div');
-        capital.innerText = country.capital;
+        countryDiv.className = 'country';
 
-        countryDiv.appendChild(name);
-        countryDiv.appendChild(capital);
+        const countryInfo = `
+                <h3 class ='country-name'>${country.name}</h3>
+                <p class = 'capital-name'>${'Capital :   ' + country.capital}</p>
+                `;
+        countryDiv.innerHTML = countryInfo;
         countriesContainer.appendChild(countryDiv);
-        
-        
-
-        // console.log(country.name);        
-    }
+    });
 }
+
+//     for (let i = 0; i < countries.length; i++) {
+//         const country = countries[i];
+//         const countryDiv = document.createElement('div');
+//         countryDiv.className = 'country';     
+
+//         const countryInfo = `
+//         <h3 class ='country-name'>${country.name}</h3>
+//         <p class = 'capital-name'>${'Capital :   ' +country.capital}</p>
+//         `;
+//         countryDiv.innerHTML = countryInfo;
+//         countriesContainer.appendChild(countryDiv);
+
+// }
