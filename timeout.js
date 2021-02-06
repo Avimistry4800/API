0627c91a68a02d6ce166471b357024686269a69d
Meghkonna Rumi <avimistry4800@gmail.com>
2021-02-07 00:05:30 +0600

    try to understand time. Fucking time is really horriable to me :(

---
+++ timeout.js
@@ -0,0 +1,12 @@
+function dosomthing(){
+    console.log(3333);    
+}
+console.log(2222);
+setTimeout(() =>{
+    console.log('See you leter.');
+},4000); // ekhane joto milisec delay dea hobe bepar ta oi time er age exicute hobe na & baki kaj ses korte jodi 1hower o lage eita oi 1 howr pore exicute hobe kintu given time er age hobe na r obosso e given timer er pore  hobe 
+console.log(4444);
+console.log(4444);
+console.log(4444);
+console.log(4444);
+console.log(4444);
