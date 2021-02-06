587b8b2328a913a5316e13db779949c2099d61ab
Meghkonna Rumi <avimistry4800@gmail.com>
2021-02-07 03:36:52 +0600

    getting more info (hoe to get & show info

--- app.js
+++ app.js
@@ -11,10 +11,29 @@ const displayCountries = countries => {
         const countryInfo = `
                 <h3 class ='country-name'>${country.name}</h3>
                 <p class = 'capital-name'>${'Capital :   ' + country.capital}</p>
+                <button onClick="displayCountryDetail('${country.name}')" >Shoe Details</button>
                 `;
         countryDiv.innerHTML = countryInfo;
         countriesContainer.appendChild(countryDiv);
     });
+}
+const displayCountryDetail = name => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => renderCountryInfo(data[0]));
+}
+
+const renderCountryInfo = country =>{
+    console.log(country);
+    const countryDiv= document.getElementById('countryDetail');
+    countryDiv.innerHTML = `
+    <h1>Name : ${country.name}</h1>
+    <p>Population : ${country.population}</p>
+    <p>Area : ${country.area}</p>
+    <img src = "${country.flag}">
+
+    `
 }
 
 //     for (let i = 0; i < countries.length; i++) {
